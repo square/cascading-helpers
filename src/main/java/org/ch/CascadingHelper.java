70d2748afbcb35621834a8612755b520ffa0c2c0
Bryan Duxbury <duxbury@squareup.com>
2012-08-24 14:36:59 -0700

    need to include the default serializations

--- src/main/java/org/ch/CascadingHelper.java
+++ src/main/java/org/ch/CascadingHelper.java
@@ -3,6 +3,7 @@ package org.ch;
 import cascading.flow.FlowConnector;
 import cascading.flow.hadoop.HadoopFlowConnector;
 import cascading.operation.aggregator.First;
+import cascading.tuple.hadoop.TupleSerialization;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -13,6 +14,7 @@ import java.util.Map;
 import java.util.Set;
 import javax.naming.directory.SchemaViolationException;
 import org.apache.hadoop.io.serializer.Serialization;
+import org.apache.hadoop.io.serializer.WritableSerialization;
 import org.apache.hadoop.mapred.JobConf;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
@@ -32,7 +34,10 @@ public class CascadingHelper {
 
   protected static final Map<Object, Object> DEFAULT_PROPERTIES = new HashMap<Object, Object>();
   protected static final List<Class<? extends Serialization>> SERIALIZATION_IMPLS =
-      new ArrayList<Class<? extends Serialization>>();
+      new ArrayList<Class<? extends Serialization>>(Arrays.asList(
+          WritableSerialization.class,
+          TupleSerialization.class
+  ));
 
   private static final int STARTING_TOKEN = 128;
   private static final List<Class> CLASSES_TO_BE_SERIALIZED = new ArrayList<Class>();
